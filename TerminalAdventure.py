87c39c9758208e995bd6ea5be6c0e0bd8c9f1a88
TechJustice101 <138499851+Huylorddd@users.noreply.github.com>
2025-04-01 17:54:18 +0700

    Add files via upload

---
+++ TerminalAdventure.py
@@ -0,0 +1,14 @@
+import random
+
+# Introduction
+print('\n\n\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/ ')
+print(' Welcome to my mini project: LEVEL UP')
+print('/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\ \n\n')
+
+# Pre-adventure
+print('----------------------------------------------')
+print(' 13/7/2025 - 7:24 P.M\n')
+print(' <> You are buying some drink after school')
+
+## monologue:
+print('<You> hmmm... which drink should I buy?')
